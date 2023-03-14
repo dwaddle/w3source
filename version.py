df6aca1190bdd2db746129c742a9dacc31ccef80
Rene <dwaddle@users.noreply.github.com>
2023-03-14 06:29:33 +0100

    version.py -- Display python version

---
+++ version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+# geeft de versie in een string
+print(sys.version)
+
+# geeft de versie in tuple vorm
+print(sys.version_info)
+
+version=sys.version_info
+
+print (version[1])
+
+
+
